fe1a7cdfec71e4d4adbc821899fcde5bf0f1713d
CamiloAranzazu18 <camilocap2018@outlook.com>
2024-07-17 14:33:00 -0500

    ajustes ordenes y menu nav

--- src/app/shared/componentsCommons/nav-menu-admin/nav-menu-admin.component.ts
+++ src/app/shared/componentsCommons/nav-menu-admin/nav-menu-admin.component.ts
@@ -1,17 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu-admin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-menu-admin.component.html',
   styleUrl: './nav-menu-admin.component.scss'
 })
 export class NavMenuAdminComponent {
 
+  toglle: boolean = false;
   @Output() clickOpcion = new EventEmitter();
   
   addToggle() {
+    this.toglle = !this.toglle;
     this.clickOpcion.emit();
   }
 }
